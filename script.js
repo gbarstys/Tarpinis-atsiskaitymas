f3d37e5556f3ab40aa6fa582c2a323bdad75ea4d
gerardasb <gerardas.barstys@gmail.com>
2022-02-04 11:09:52 +0200

    commit3

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
-"use strict";
+// window.onload = function () {
+//     $(document).ready(function () {
+//         function changephotostyle(x) {
+//             x.classList.add("tabfthover");
+//             var photo = document.getElementsByClassName("photo-text");
+//             photo.classList.add("photo-texthover");
+//             // const html = document.createElement('span');
+//             // html.className = 'photo-text photo-texthover';
+//             // html.innerHTML = 'Explore';
+//             // x.appendChild(html); 
+//         }
+//         function changephotoback(x) {
+//             x.classList.remove("tabfthover");
+//             var photo = document.getElementsByClassName("photo-text");
+//             photo.classList.remove("photo-texthover");
+
+//         }
+//     });
+// };
+
+function changephotostyle(x) {
+    x.classList.add("tabfthover");
+    // var photo = document.getElementsByClassName("photo-text");
+    // photo.classList.add("photo-texthover");
+    // const html = document.createElement('span');
+    // html.className = 'photo-text photo-texthover';
+    // html.innerHTML = 'Explore';
+    // x.appendChild(html); 
+}
+function changephotoback(x) {
+    x.classList.remove("tabfthover");
+        // var photo = document.getElementsByClassName("photo-text");
+        // photo.classList.remove("photo-texthover");
+}
 
 // Navigation menu 
 function myFunction(){
@@ -17,3 +50,18 @@ function myFunction(){
     function changeback(x) {
         x.classList.remove("tabhover");
     }
+
+    // container 2 antri tabai
+
+
+    //swiperis
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+});
